6aa785d96afd9b8d728d482ae53aaafff045329e
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-01-04 20:39:37 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './Layout'
-import Home from './Components/Home/Home'
-import About from './Components/About/About'
-import Cheese from './Components/Cheese/Cheese'
-import Contact from './Components/Contact/Contact'
-import Fruit from './Components/Fruit/Fruit'
-import Seasonal from './Components/Seasonal/Seasonal'
-import Pies from './Components/Pies/Pies'
+import App from './App'
 
-const router=createBrowserRouter([{
-  path:'',
-  element:<Layout/>,
-  children:[{
-    path:'/',
-    element:<Home />
-  },
-  {
-    path:'/About',
-    element:<About/>
-  },{
-    path:'/Cheese',
-    element:<Cheese/>
-  },
-{
-  path:'/Contact',
-  element:<Contact/>
-},{
-  path:'/Fruit',
-  element:<Fruit/>
-},{
-  path:'/Seasonal',
-  element:<Seasonal/>
-},{
-  path:'/Pies',
-  element:<Pies/>
-}]
-}])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router}/>
+  <App/>
   </StrictMode>,
 )
