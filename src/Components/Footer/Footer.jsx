bb64c2acaffbc20c5655524a5f179f574601c35f
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-02-20 17:06:58 +0500

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 function Footer() {
   return (
    <>
-    <footer className="w-[631px] sm:w-[980px] md:w-[1186px] lg:w-[1394.4px] xl:w-[1582px] h-[152px] bg-rose-300  flex mt-40 lg:mt-10">
+    <footer className="w-auto sm:w-[980px] md:w-[1186px] lg:w-[1394.4px] xl:w-[1582px] h-[152px] bg-rose-300  flex mt-40 lg:mt-10">
       <div className="text-start pl-5 pt-5 font-sans text-[16px]">
         <p>Our address is</p>
         <p>Bethany's Pie Shop - Bakery Street 5555 Brussels, Belgium</p>
