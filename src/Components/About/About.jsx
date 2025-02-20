74be564a6316ba37bd3c0e87f91b2c73e0768cef
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-02-20 17:19:17 +0500

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom'
 function About() {
   return (
     <>
-    <div className="main1  w-[550px] md:w-[720px] lg:w-[855px] xl:w-[990px]  mt-8 ml-12 ">
-            <h1 className="w-[498.4px] h-[44.8px] mb-10 text-[40px] font-sans font-bold text-amber-600">About Bethany's Pie Shop</h1>
-            <p className="w-[479px] sm:w-[550px] md:w-[720px] lg:w-[831px] xl:w-[966px] h-[24px] mb-4 font-sans text-[16px]">In the video below, you can see how we create our pies.</p>
+    <div className="main1  w-auto md:w-[720px] lg:w-[855px] xl:w-[990px]  mt-8 ml-12 ">
+            <h1 className="w-auto h-[44.8px] mb-10 text-[40px] font-sans font-bold text-amber-600">About Bethany's Pie Shop</h1>
+            <p className="w-auto sm:w-[550px] md:w-[720px] lg:w-[831px] xl:w-[966px] h-[24px] mb-4 font-sans text-[16px]">In the video below, you can see how we create our pies.</p>
             {/* <!-- <video class="w-[942px] h-[270px]" controls loop src="https://youtu.be/jWgMt5GCo7U">
                 <source src="https://youtu.be/jWgMt5GCo7U" type="video/mp4">
             </video> --> */}
