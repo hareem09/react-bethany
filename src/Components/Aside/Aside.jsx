b76c47e2324a2a36668d880704ac34c74912d13b
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-02-20 17:05:10 +0500

    Update Aside.jsx

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -4,12 +4,12 @@ function Aside() {
   return (
    <>
    
-    <div className="aside w-[250px]  flex flex-col font-sans  text-center align-middle md:ml-40 ">
-        <img src="Images\bethany-badge-logo.png" alt="" className="logo2 mt-14 ml-20 "/>
-        <h5 className="heading mt-3 self-center text-bold text-SM w-48 h-6 ml-20">
+    <div className="aside w-44 md:w-[250px]  flex flex-col font-sans  text-center align-middle md:ml-40 ">
+        <img src="Images\bethany-badge-logo.png" alt="" className="logo2  mt-14 ml-20 "/>
+        <h5 className="heading mt-3 self-center text-bold text-sm w-48 h-6 ml-20">
             <b>BROWSE OUR PICTURES</b>
         </h5>
-        <ul className="text-center underline text-amber-600 ml-20">
+        <ul className="text-center underline text-amber-600 ml-14 md:ml-20">
             <li><Link to='/Pies' >ALL PIES</Link></li>
             <li><Link to='/Cheese'>CHEESE CAKES</Link></li>
             <li><Link to='/Fruit'>FRUIT PIES</Link></li>
